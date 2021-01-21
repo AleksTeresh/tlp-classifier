20e4526595c655f7231c9944fdced4dbf2b3cadf
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-01-21 19:17:07 +0200

    Add missing import to package's __init__ file

--- tlp_classifier/__init__.py
+++ tlp_classifier/__init__.py
@@ -4,6 +4,7 @@ from .two_labels_classifier import *
 from .problem_set import *
 from .problem import *
 from .classifier import *
+from .generator import *
 
 # Package version
 __version__ = '0.1.0'
